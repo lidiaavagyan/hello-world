c9722e6e5af835ec8b1f1cfeba637c316bc63f78
lidiaavagyan <147895972+lidiaavagyan@users.noreply.github.com>
2023-10-14 18:49:01 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_WorldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет,");
+            Console.Write("Мир");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
